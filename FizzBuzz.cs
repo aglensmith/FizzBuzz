aff95c8458a2e2229bb3befd8a1f862c6c8e0377
A G Smith <aglensmith@gmail.com>
2015-07-05 12:59:49 -0500

    Solves using for loop and conditionals

--- FizzBuzz.cs
+++ FizzBuzz.cs
@@ -10,6 +10,25 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
+            for (int i = 1; i < 101; i++)
+            {
+                if (i % 3 == 0 && i % 5 != 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                if (i % 5 == 0 && i % 3 != 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i % 3 != 0 && i % 5 != 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
         }
     }
 }
